f790a8750f95fc7347afcce388cdc676913c950a
Cuong Nguyen <cuong.nguyen@topdatascience.com>
2022-10-09 23:54:24 +0300

    Init project

---
+++ unocss.config.ts
@@ -0,0 +1,33 @@
+import {
+	defineConfig,
+	presetIcons,
+	presetTypography,
+	presetUno,
+	presetWebFonts,
+	transformerDirectives,
+	transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+	shortcuts: [],
+	presets: [
+		presetUno(),
+		presetIcons({
+			scale: 1.2,
+			warn: true,
+		}),
+		presetTypography(),
+		presetWebFonts({
+			fonts: {
+				sans: 'PT Sans',
+				serif: 'PT Serif',
+				mono: 'PT Mono',
+			},
+		}),
+	],
+	transformers: [
+		transformerDirectives(),
+		transformerVariantGroup(),
+	],
+	safelist: 'prose prose-sm m-auto text-left'.split(' '),
+})
